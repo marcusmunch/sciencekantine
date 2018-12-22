1781eeb23af15e1b21f65de56b1bfcab784f6ef7
Marcus Munch <Marcus@MarcusMunch.dk>
2018-12-23 00:53:49 +0100

    Reworked motd.py

--- motd.py
+++ motd.py
@@ -1,13 +1,36 @@
 import datetime
+import sys
 
 from argparse import ArgumentParser
 
 import kukantine
 
 
-def motd(sep=", "):
+def get_motd(canteen, debug=False, sep=", "):
     today_weekday = datetime.datetime.today().weekday()
 
+    if not debug:
+        menu = kukantine.get_menu_week(canteen)
+
+    else:
+        if today_weekday > 4:
+            today_weekday = 4
+
+        menu = kukantine.load_menu_from_json(canteen)
+
+    print(menu)
+
+    try:
+        motd = menu[today_weekday]
+
+    except IndexError:
+        print("An error occured. This might be because it's the weekend today.")
+        sys.exit(1)
+
+    return sep.join(["%s: %s" % (k, v) for k, v in motd.items()])
+
+
+def main():
     parser = ArgumentParser()
     parser.add_argument("-s", "--separator", help="String to use for separation of the different menu items. "
                                                   "Defaults to \", \".")
@@ -15,20 +38,12 @@ def motd(sep=", "):
     parser.add_argument("-d", help="Debug mode", action="store_true")
     args = parser.parse_args()
 
-    if not args.d:
-        menu = kukantine.get_menu_week(args.canteen)
+    if args.d:
+        debug = True
     else:
-        import json
-        with open(args.canteen.lower() + ".json") as f:
-            menu = json.load(f)[args.canteen]
+        debug = False
 
-    motd = menu[today_weekday]
-
-    return sep.join(["%s: %s" % (k, v) for k, v in motd.items()])
-
-
-def main():
-    print(motd())
+    print(get_motd(args.canteen, debug=debug))
 
 
 if __name__ == "__main__":
